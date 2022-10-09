3199ee20a40457fcd937985570fe8a64103414ce
Awwal Aderibigbe <mac@macs-MacBook-Pro.local>
2022-10-09 11:25:50 +0100

    commits

---
+++ Fzscraper_app/wsgi.py
@@ -0,0 +1,21 @@
+"""
+WSGI config for Fzscraper_app project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+from whitenoise import WhiteNoise
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Fzscraper_app.settings')
+
+application = get_wsgi_application()
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+application = WhiteNoise(application, root=os.path.join(BASE_DIR,'/static'))
